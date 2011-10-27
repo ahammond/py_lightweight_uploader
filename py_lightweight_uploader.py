bea53598f5f78590572b9b630d99a899ef657596
Andrew Hammond <andrew.george.hammond@gmail.com>
2011-10-27 13:04:10 -0700

    cleaned up / corrected license section in code

--- py_lightweight_uploader.py
+++ py_lightweight_uploader.py
@@ -14,8 +14,31 @@ from uuid import uuid4
 
 __author__ = 'Andrew Hammond <andrew.hammond@receipt.com>'
 __copyright__ = 'Copyright (c) 2011 SmartReceipt'
-#TODO: what license? Should match ngnix, I guess.
-__license__ = 'GNU GPL v2???'
+__license__ = """Standard 2 clause BSD (matches nginx license)
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of
+      conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list
+      of conditions and the following disclaimer in the documentation and/or other materials
+      provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY SmartReceipt Inc. ''AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SmartReceipt Inc. OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those of the
+authors and should not be interpreted as representing official policies, either expressed
+or implied, of SmartReceipt Inc.
+"""
 __vcs_id__ = '$Id$'
 
 
